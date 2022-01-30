43ee20f17e15f9a780d11c6862e25d5fcb3eb2c4
andrei <12499409+andreiTn@users.noreply.github.com>
2022-01-30 09:10:22 +0200

    update theme colour

--- quasar.conf.js
+++ quasar.conf.js
@@ -154,7 +154,7 @@ module.exports = configure(function (ctx) {
         display: 'standalone',
         orientation: 'portrait',
         background_color: '#ffffff',
-        theme_color: '#027be3',
+        theme_color: '#FD7272',
         icons: [
           {
             src: 'icons/icon-128x128.png',
